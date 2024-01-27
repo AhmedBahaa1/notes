bef7b664781f13c7eeae5f1bc6cf4c9664c798b2
AhmedBahaa1 <ah.bahaa@outlook.com>
2024-01-27 14:37:01 +0200

    creat text field and add constant kPrimaryColor to app

--- lib/widgets/add_note_botton_sheet.dart
+++ lib/widgets/add_note_botton_sheet.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:notes/widgets/custom_text_field.dart';
 
 class AddNoteFromBottonSheet extends StatelessWidget {
   const AddNoteFromBottonSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        // child: Text('data'),
-        );
+    return Column(
+      children: [
+        SizedBox(
+          height: 32,
+        ),
+        CustomTextField(),
+      ],
+    );
   }
 }

7a107fa4f64cc3efb7e06b358a803530fde4b623
AhmedBahaa1 <ah.bahaa@outlook.com>
2024-01-27 15:36:37 +0200

    create edit note view

--- lib/widgets/custom_card_item.dart
+++ lib/widgets/custom_card_item.dart
@@ -1,51 +1,60 @@
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes/views/edit_note_view.dart';
 
 class CardItem extends StatelessWidget {
   const CardItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24, bottom: 25, left: 16),
-      decoration: BoxDecoration(
-        color: Colors.amber,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter tips',
-              style: TextStyle(color: Colors.black, fontSize: 30),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: Text(
-                'Learn Flutter with Sarwat samy',
-                style: TextStyle(
-                    fontSize: 18, color: Colors.black.withOpacity(0.4)),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) {
+            return EditNoteView();
+          },
+        ));
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24, bottom: 25, left: 16),
+        decoration: BoxDecoration(
+          color: Colors.amber,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutter tips',
+                style: TextStyle(color: Colors.black, fontSize: 30),
               ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  'Learn Flutter with Sarwat samy',
+                  style: TextStyle(
+                      fontSize: 18, color: Colors.black.withOpacity(0.4)),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 24,
+                  )),
             ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 24,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May 2 , 1994',
-              style:
-                  TextStyle(fontSize: 16, color: Colors.black.withOpacity(0.4)),
-            ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May 2 , 1994',
+                style: TextStyle(
+                    fontSize: 16, color: Colors.black.withOpacity(0.4)),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }

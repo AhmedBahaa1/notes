578587c409f342effb1ddce64be7bfd4ca19dec7
AhmedBahaa1 <ah.bahaa@outlook.com>
2024-01-27 14:45:30 +0200

    add field note booton sheet

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:notes/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
+  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
+  final String hint;
+  final int maxLines;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: TextField(
         cursorColor: kPrimaryColor,
+        maxLines: maxLines,
         decoration: InputDecoration(
-            hintText: 'Title',
+            hintText: hint,
             border: buildBorder(),
             enabledBorder: buildBorder(),
             focusedBorder: buildBorder(kPrimaryColor)),

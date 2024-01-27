2d187e709e1dfa52d85d653f59df4f299c137446
AhmedBahaa1 <ah.bahaa@outlook.com>
2024-01-27 11:19:37 +0200

    create card item and add font poppins and fontAwsome for icon

--- lib/widgets/notes_app_body.dart
+++ lib/widgets/notes_app_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes/widgets/custom_app_bar.dart';
+import 'package:notes/widgets/custom_card_item.dart';
 
 class NotesAppBody extends StatelessWidget {
   const NotesAppBody({super.key});
@@ -7,17 +9,21 @@ class NotesAppBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 24,
-        ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 60,
-            ),
-            CustomAppBar(),
-          ],
-        ),
-      );
+      padding: EdgeInsets.symmetric(
+        horizontal: 24,
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 60,
+          ),
+          CustomAppBar(),
+          SizedBox(
+            height: 16,
+          ),
+          CardItem(),
+        ],
+      ),
+    );
   }
 }
